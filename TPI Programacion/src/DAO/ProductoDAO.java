0a878bfa36cfbe9ccf2dbba5256c4da45efd8791
Tu Nombre <tu@correo.com>
2025-11-15 23:59:26 -0300

    nombres

--- TPI Programacion/src/DAO/ProductoDAO.java
+++ TPI Programacion/src/DAO/ProductoDAO.java
@@ -1,6 +1,12 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ /**
+ * @authors 
+ * Gaston Alberto Cejas, 
+ * Hernan Cóceres, 
+ * Claudio Rodriguez, 
+ * Hernan E.Bula
  */
 package DAO;
 
